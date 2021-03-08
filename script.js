69eb2f39cdf66203d471e66530d6f6456637afc9
ID-JA <69154853+ID-JA@users.noreply.github.com>
2021-03-08 10:49:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const attribution =
+	'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+const tilesUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer(tilesUrl, { attribution, noWrap: true });
+
+const map = L.map('map', {
+	minZoom: 1,
+	worldCopyJump: false,
+	maxBounds: null,
+}).setView([0, 0], 1);
+const issIcon = L.icon({
+	iconUrl: 'iss.png',
+	iconSize: [80, 80],
+});
+
+const marker = L.marker([0, 0], { icon: issIcon }).addTo(map);
+
+async function getCoords() {
+	const response = await fetch(
+		'https://api.wheretheiss.at/v1/satellites/25544'
+	);
+
+	const data = await response.json();
+	console.log(data);
+	const { latitude, longitude } = data;
+	marker.setLatLng([latitude, longitude]);
+
+	document.getElementById('lat').innerHTML = `latitude: ${latitude.toFixed(2)}`;
+	document.getElementById('lon').innerHTML = `longitude: ${longitude.toFixed(
+		2
+	)}`;
+}
+setInterval(() => getCoords(), 1000);
+tiles.addTo(map);
